95a6e7faae92676975b638cbee04c9a32ab942a2
Alessandro Rossi <S4641902@studenti.unige.it>
2022-02-10 20:41:49 +0100

    Implemented Generic repository pattern

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+    }
+}
---
+++ Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class GenericRepository <T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly BakeryContext _context;
+        public GenericRepository(BakeryContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+    }
+}
